e9186360d9bea419100a80c5dd990b6c6e51ea1f
Rupertt Wind aka Aravind Anilkumar <ruperttaryeenwind@gmail.com>
2020-04-16 19:39:04 +0530

    [base] Create example components with prop drilling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Family = (props) => (
+  <div>
+    <Person name={props.name} />
+  </div>
+);
+
+class Person extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <p>Hey, I am { this.props.name }</p>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    name: "Rupertt",
+    age: 29
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Family name={ this.state.name } />
+      </div>
+    );
+  }
 }
 
 export default App;
