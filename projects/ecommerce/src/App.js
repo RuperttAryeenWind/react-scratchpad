74c038938bb5d017888e747445e645caef1da34b
RuperttAryeenWind <ruperttaryeenwind@gmail.com>
2020-09-27 10:32:44 +0530

    [ecommerce] Adding a basic product card

--- projects/ecommerce/src/App.js
+++ projects/ecommerce/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Product from './components/product/Product';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const products = [
+  {
+    productId: 1001,
+    productCategory: 'Category 3',
+    productName: 'Product 1',
+    productImage: 'https://picsum.photos/400?image=768',
+    productStock: false,
+    productPrice: 20478.708,
+    salePrice: 17611.688879999998
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {
+        products.map((product) => (
+          <Product product={product} />
+        ))
+      }
     </div>
   );
 }
